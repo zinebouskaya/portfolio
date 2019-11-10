e0115467430ea2b5fab1fadcc143fc1c7c51488f
zinebouskaya <zine.bouskaya@gmail.com>
2019-11-10 19:55:40 +0100

    Final version

--- experiences/models.py
+++ experiences/models.py
@@ -4,4 +4,4 @@ from django.db import models
 # Create your models here.
 class experiences(models.Model):
     image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200)
+    summary = models.TextField()
